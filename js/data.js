d0409a1f8b9c5bbc253f061fd3dc086df576a1bb
Bryan Wong <bryan@jms-wong.local>
2017-08-02 16:12:03 -0700

    file cleanup, added loading screen

--- js/data.js
+++ js/data.js
@@ -3,17 +3,6 @@ var GEOMETRIES = {
 	current : 0
 };
 
-var gunGeom;
-var modelLoader = new THREE.JSONLoader();
-modelLoader.load(
-	'assets/gun.json',
-
-	function(geometry, materials){
-		gunGeom = geometry;
-		GEOMETRIES.Array.push(gunGeom);
-	}
-);
-
 var sphereGeom = new THREE.SphereGeometry(2.5, 128, 128);
 var SPHERE_VERTICES = sphereGeom.vertices;
 GEOMETRIES.Array.push(sphereGeom);
